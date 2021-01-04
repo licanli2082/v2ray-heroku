45499ea8f872551153c38ed944af30910a0130c1
licanli2082 <licanli2082@qq.com>
2021-01-04 23:52:20 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,15 +4,15 @@
 mkdir /tmp/v2ray
 curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
-install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
-install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+install -m 755 /tmp/v2ray/v2ray /tmp/local/bin/v2ray
+install -m 755 /tmp/v2ray/v2ctl /tmp/local/bin/v2ctl
 
 # Remove temporary directory
 rm -rf /tmp/v2ray
 
 # V2Ray new configuration
-install -d /usr/local/etc/v2ray
-cat << EOF > /usr/local/etc/v2ray/config.json
+install -d /tmp/local/etc/v2ray
+cat << EOF > /tmp/local/etc/v2ray/config.json
 {
     "inbounds": [
         {
@@ -40,5 +40,5 @@ cat << EOF > /usr/local/etc/v2ray/config.json
 }
 EOF
 
-# Run V2Ray
-/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
+#Run V2Ray
+/tmp/local/bin/v2ray -config /tmp/local/etc/v2ray/config.json
